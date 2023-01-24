d96ae1775d13e65783c8fbb668c7045ddb7efb8e
Maxime Amodei <maxime.amodei@uliege.be>
2023-01-24 15:13:32 +0100

    feat: compute TRE for point annotations

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ import shapely
 import shapely.errors
 import shapely.wkt
 from shapely.affinity import affine_transform
+from shapely.geometry.point import Point
 from shapely.ops import transform
 from shapely.validation import make_valid
 from valis import registration
@@ -347,6 +348,21 @@ def iou_annotations(
     return inter / union
 
 
+def tre_annotations(
+    left: cm.Annotation,
+    right: cm.Annotation,
+) -> float:
+    "compute TRE (L2 distance) for two Point annotations"
+
+    geometry_l = shapely.wkt.loads(left.location)
+    geometry_r = shapely.wkt.loads(right.location)
+
+    assert isinstance(geometry_l, Point)
+    assert isinstance(geometry_r, Point)
+
+    return geometry_l.distance(geometry_r)
+
+
 class VALISJob(typing.NamedTuple):
 
     cytomine_job: cytomine.CytomineJob
@@ -531,7 +547,9 @@ class VALISJob(typing.NamedTuple):
             "evaluation on %d annotation groups", len(group.eval_annotation_groups)
         )
 
-        evaluation_rows = []
+        # metric measurements
+        tre_ = tre_annotations, []
+        iou_ = iou_annotations, []
 
         for an_group in group.eval_annotation_groups:
             an_coll = cm.AnnotationCollection(group=an_group.id)
@@ -556,7 +574,10 @@ class VALISJob(typing.NamedTuple):
                 if not src_img:
                     raise ValueError(f"cannot fetch ImageInstance with id={an.image}")
 
-                iou_lst: typing.List[float] = []
+                if isinstance(shapely.wkt.loads(an.location), Point):
+                    metric, rows = tre_
+                else:
+                    metric, rows = iou_
 
                 an_gt: cm.Annotation
                 for an_gt in an_coll:
@@ -574,7 +595,7 @@ class VALISJob(typing.NamedTuple):
 
                     # IoU between gt and pred
                     try:
-                        iou = iou_annotations(pred, an_gt)
+                        iou = metric(pred, an_gt)
                     except shapely.errors.ShapelyError as e:
                         self.logger.error(
                             "unable to compute IoU between an=%d (img=%d) and "
@@ -591,31 +612,55 @@ class VALISJob(typing.NamedTuple):
                         self.logger.exception(e)
                         continue
 
-                    evaluation_rows.append(
+                    rows.append(
                         (an_group.id, an.id, src_img.id, an_gt.id, dst_img.id, iou)
                     )
 
-        filename = group.image_group.name + f"-{group.image_group.id}-iou.csv"
-        path = str(self.base_dir / filename)
-        pd.DataFrame(
-            evaluation_rows,
-            columns=[
-                "annotation_group",
-                "annotation_src",
-                "image_src",
-                "annotation_gt",
-                "image_gt",
-                "iou",
-            ],
-        ).to_csv(path)
+        if iou_[1]:
+            filename = group.image_group.name + f"-{group.image_group.id}-iou.csv"
+            path = str(self.base_dir / filename)
+            pd.DataFrame(
+                iou_[1],
+                columns=[
+                    "annotation_group",
+                    "annotation_src",
+                    "image_src",
+                    "annotation_gt",
+                    "image_gt",
+                    "iou",
+                ],
+            ).to_csv(path)
 
-        job_data = cm.JobData(
-            id_job=self.cytomine_job.job.id,
-            key=f"IoU ({group.image_group.id})",
-            filename=filename,
-        )
-        job_data = job_data.save()
-        job_data.upload(path)
+            job_data = cm.JobData(
+                id_job=self.cytomine_job.job.id,
+                key=f"IoU ({group.image_group.id})",
+                filename=filename,
+            )
+            job_data = job_data.save()
+            job_data.upload(path)
+
+        if tre_[1]:
+            filename = group.image_group.name + f"-{group.image_group.id}-tre.csv"
+            path = str(self.base_dir / filename)
+            pd.DataFrame(
+                tre_[1],
+                columns=[
+                    "annotation_group",
+                    "annotation_src",
+                    "image_src",
+                    "annotation_gt",
+                    "image_gt",
+                    "tre",
+                ],
+            ).to_csv(path)
+
+            job_data = cm.JobData(
+                id_job=self.cytomine_job.job.id,
+                key=f"TRE ({group.image_group.id})",
+                filename=filename,
+            )
+            job_data = job_data.save()
+            job_data.upload(path)
 
     def predict(self, group: RegistrationGroup, registrar: registration.Valis):
 
